6b5ca3da5f4f4f1e59d5f668ee7d5c127070d833
WHOequity <baniropocr@throam.com>
2015-12-11 23:46:00 +0800

    Moved the "Ready" checkbox to the bottom of the landing page.  Sdded a slider to vary the HbA1c cutoff

--- ui.R
+++ ui.R
@@ -4,21 +4,31 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     
-              
+    
     sidebarPanel(
       
-      checkboxInput(inputId = "proceed", label = "Ready to start?", value = FALSE, width = NULL),
       
       conditionalPanel(condition = "input.proceed == true",
-                       sliderInput(inputId = "fbg", "Fasting Plasma Blood Glucose",  
-                                   min = 2, max = 22, value = 7.4, step = 0.1)
+                       sliderInput(inputId = "fbg", "Fasting Blood Glucose",  
+                                   min = 2, max = 22, value = 7.4, step = 0.1),
+                       sliderInput(inputId = "cutpoint", "HbA1c cut-off",  
+                                   min = 40, max = 100, value = 69.4, step = 0.1)
       )
     ),
     
     mainPanel(
-      conditionalPanel(condition="input.proceed == false", includeHTML("./landing_page.html"))
-#       plotOutput("logisticPlot"),
-#       plotOutput("rocPlot")
+      conditionalPanel(condition="input.proceed == false", 
+                       plotOutput("linearPlot"),
+                       includeHTML("./landing_page.html"),
+                       checkboxInput(inputId = "proceed", 
+                                     label = "Ready to start?", 
+                                     value = FALSE, width = NULL)
+                       
+                       ),
+      conditionalPanel(condition="input.proceed == true",
+                       plotOutput("logisticPlot")
+      )
+      #       plotOutput("rocPlot")
     )
   )
 ))
