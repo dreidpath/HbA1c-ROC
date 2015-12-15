0943bba69b114f2050a52a6f8e00faf0a8d6f3a5
WHOequity <baniropocr@throam.com>
2015-12-15 08:33:53 +0800

    The HTML for the binary classification / confusion matrix

---
+++ classification-matrix.R
@@ -0,0 +1,69 @@
+binclassmat <- '<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;margin:0px auto; }
+.tg td{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px;}
+.tg th{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px;}
+.tg .tg-topbottompad{padding:30px 30px;}
+.tg .tg-tabletitle{background-color: gray; color: white; font-family:Arial, sans-serif;font-size: 1.5em; font-weight: bold;}
+.tg .tg-actualtop{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px; font-weight: bold; border-top:3px; border-bottom:0px; border-left:0px; border-right:0px; border-color:black; border-style:solid;}
+.tg .tg-actual{font-family:Arial, sans-serif;font-size: 1.1em; padding:10px 5px; font-weight: bold;}
+.tg .tg-titlerow{font-family:Arial, sans-serif; font-size: 1em; padding:10px 5px; font-weight: bold; text-align: center;}
+.tg .tg-titlerowpred{font-family:Arial, sans-serif; font-size: 1em; padding:10px 5px; font-weight: bold; text-align: center;}
+.tg .tg-matrixtop{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px; border-top:3px; border-bottom:0px; border-left:0px; border-right:0px; border-color:black; border-style:solid; text-align: center;}
+.tg .tg-matrixbottomtitle{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px;  border-top:0px; border-bottom:2px; border-left:0px; border-right:0px; border-color:black; border-style:solid;}
+.tg .tg-matrixbottom{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px;  border-top:0px; border-bottom:2px; border-left:0px; border-right:0px; border-color:black; border-style:solid; text-align: center;}
+.tg .tg-numbers{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px; text-align: center;}
+.tg .tg-cmtitlebottom{font-family:Arial, sans-serif;font-size: 1em;font-weight:normal;padding:10px 5px;  border-top:0px; border-bottom:3px; border-left:0px; border-right:0px; border-color:black; border-style:solid;}
+.tg .tg-cmresult{text-align: center;}
+</style>
+
+
+<div class="tg-wrap"><table class="tg">
+<tr>
+<th class="tg-topbottompad" colspan="3"></th>
+</tr>
+<tr>
+<th class="tg-tabletitle" colspan="3">Binary Classification Matrix: <br></th>
+</tr>
+<tr class="tg-actualtop">
+<th></th>
+<th   class="tg-actual"colspan="2">Actual<br></th>
+</tr>
+<tr class="tg-titlerow">
+<td class="tg-titlerowpred">Predicted</td>
+<td>Positive<br></td>
+<td>Negative</td>
+</tr>
+<tr>
+<td class="tg">Positive</td>
+<td class="tg-matrixtop">CELL 1</td>
+<td class="tg-matrixtop">CELL 2</td>
+</tr>
+<tr>
+<td class="tg-matrixbottomtitle">Negative</td>
+<td class="tg-matrixbottom">CELL 3</td>
+<td class="tg-matrixbottom">CELL 4</td>
+</tr>
+<tr>
+<td class="tg-cmtitle">Sensitivity</td>
+<td class="tg-cmresult" colspan="2">CELL 5</td>
+</tr>
+<tr>
+<td class="tg-cmtitle">Specificity</td>
+<td class="tg-cmresult" colspan="2">CELL 6</td>
+</tr>
+<tr>
+<td class="tg-cmtitle">Accuracy</td>
+<td class="tg-cmresult" colspan="2">CELL 7</td>
+</tr>
+<tr>
+<td class="tg-cmtitle">HbA1c cutpoint</td>
+<td class="tg-cmresult" colspan="2">CELL 8</td>
+</tr>
+<tr class="tg-cmtitlebottom">
+<td class="tg-cmtitle">Blood Glucose</td>
+<td class="tg-cmresult" colspan="2">CELL 9</td>
+</tr>
+<tr>
+<th class="tg-topbottompad" colspan="3"></th>
+</tr>
+</table></div>'
